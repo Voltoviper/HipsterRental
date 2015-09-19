565e75796628179c66e509cfe1dd250cf3c08afc
Christoph Nebendahl <christoph@mail-nebendahl.de>
2015-09-19 14:03:31 +0200

    Initial commit

---
+++ src/wak/Home.java
@@ -0,0 +1,11 @@
+package wak;
+
+/**
+ * Created by Christoph Nebendahl on 19.09.2015.
+ */
+public class Home {
+
+    public static void main(String args[]){
+        System.out.print("Test");
+    }
+}
