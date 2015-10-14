58b9f20edf91882ac7b4a6ea0d19486d6c61d8f0
nelta <nelta.gaming@web.de>
2015-10-14 15:15:31 +0200

    UML Class Diagramm

--- src/wak/objects/Produkt.java
+++ src/wak/objects/Produkt.java
@@ -8,15 +8,12 @@ import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Arrays;
 
-/**
- * Created by Christoph Nebendahl on 20.09.2015.
- */
 public class Produkt {
     String name, bezeichnung, beschreibung, herstellername, details;
     double mietzins;
     Produkt alternative;
     Kategorie kategorie;
-    String [] bilder;
+    String[] bilder;
     Geraet[] geraete;
     int id;
 
